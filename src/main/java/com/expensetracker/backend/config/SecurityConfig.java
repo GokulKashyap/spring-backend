2e14ac0802b295818bc07b7d175d7d27afe8e559
GokulKashyap <gokulkashyapgannamaraju@gmail.com>
2025-06-15 14:45:18 +0530

    SecurityConfig

--- src/main/java/com/expensetracker/backend/config/SecurityConfig.java
+++ src/main/java/com/expensetracker/backend/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        .cors(Customizer.withDefaults()) 
+          .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/login", "/api/auth/register").permitAll() 
