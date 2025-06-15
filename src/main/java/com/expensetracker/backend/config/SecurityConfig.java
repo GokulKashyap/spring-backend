1a0f26d17aab1a70559b89429d5449e4951d48b1
GokulKashyap <gokulkashyapgannamaraju@gmail.com>
2025-06-15 14:00:05 +0530

    Changes in config

--- src/main/java/com/expensetracker/backend/config/SecurityConfig.java
+++ src/main/java/com/expensetracker/backend/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
      @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // ✅ Your frontend origin
+        config.setAllowedOrigins(List.of("https://daily-budget-buddy.netlify.app")); // ✅ Your frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Required if you send cookies or Authorization headers
